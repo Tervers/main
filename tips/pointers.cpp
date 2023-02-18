4fad9f559b49a570a3da7ad5673fc8ba997ebc10
Tervers <tcf847@protonmail.com>
2023-02-18 15:53:17 -0800

    /tips/pointers.cpp updated

--- tips/pointers.cpp
+++ tips/pointers.cpp
@@ -1,7 +1,11 @@
 int i = 42;
-int &r = i;   // & follows a type and is part of a declaration; r is a reference
-int *p;       // * follows a type and is part of a declaration; p is a pointer
-p = &i;       // & is used in an expression as the address-of operator
-*p = i;       // * is used in an expression as the dereference operator
-int *p = i;   /* invalid */
-int &r2 = *p; // & is part of the declaration; * is the dereference operator
+int &r = i;   	 // & follows a type and is part of a declaration; r is a reference
+int *p;       	 // * follows a type and is part of a declaration; p is a pointer
+p = &i;       	 // & is used in an expression as the address-of operator
+*p = i;       	 // * is used in an expression as the dereference operator
+int *p = i;   	 /* invalid */
+int &r2 = *p; 	 // & is part of the declaration; * is the dereference operator
+int* p1, p2; 	 // p1 is a pointer to int; p2 is an int
+	int *p1, *p2;  // both p1 and p2 are pointers to int - \
+	int* p1;       // p1 is a pointer to int - - - - - - - - > both styles can define pointers/references
+	int* p2;       // p2 is a pointer to int - - - - - - - /
