4dd6c92436083470a16c4f002cd5c9fdd0b5b5a3
Tervers <tcf847@protonmail.com>
2023-02-18 15:31:35 -0800

    added /tips directory

---
+++ tips/pointers.cpp
@@ -0,0 +1,7 @@
+int i = 42;
+int &r = i;   // & follows a type and is part of a declaration; r is a reference
+int *p;       // * follows a type and is part of a declaration; p is a pointer
+p = &i;       // & is used in an expression as the address-of operator
+*p = i;       // * is used in an expression as the dereference operator
+int *p = i;   /* invalid */
+int &r2 = *p; // & is part of the declaration; * is the dereference operator
