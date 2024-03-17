0e864121d47d06e662cd00f0ac7e52714c742383
Trevor Freeman <tcf847@protonmail.com>
2024-03-17 10:14:43 -0700

    test change

--- code/for4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-	int sum = 0;
-	for (int i = -100; i <= 100; ++i); std::cout << i;
-		sum += i;
-	std::cout << "1 to 200 inclusive is " << sum << std::endl;
-	return 0;
-}
--- code/literals2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main()
-{
-	std::cout << "2\x4d\012" << std::endl;
-	std::cout << "2\t\115\012" << std::endl;
-	return 0;
-}
