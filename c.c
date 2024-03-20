c11817a3d519ac82fda2405bc7395cbadc622b3c
Trevor Freeman <tcf847@protonmail.com>
2024-03-20 10:29:07 -0700

    scanf and references

--- c.c
+++ c.c
@@ -7,7 +7,7 @@
 * int main(void)            // int = variable type. implies main returns an integer / status code.
 *                           // void = main has no argument.
 * {
-* printf("I love \n");      // printf = statement. library function call.
+* printf("I love \n");      // printf = 'print formatted' statement. library function call.
 * printf("you Michelle.\n");// statements end with a ;.
 * return 0;                 // return = statement. value.
 * }
@@ -71,3 +71,29 @@ int main(void)
 // can also be written as:
 
 // printf("%d\n", height * length * width);
+
+
+
+ #include <stdio.h>
+
+ int main(void)
+
+{
+
+   int height, length, width, volume, weight;
+
+   printf("Enter height of box: ");
+   scanf("%d", &height);   // 'scan formatted' function to accept user input.
+   printf("Enter length of box: ");
+   scanf("%d", &length);
+   printf("Enter width of box: ");
+   scanf("%d", &width);
+   volume = height * length * width;
+   weight = (volume + 165) / 166;
+
+   printf("Volume (cubic inches): %d\n", volume);
+   printf("Dimensional weight (pounds): %d\n", weight);
+
+   return 0;
+
+}
