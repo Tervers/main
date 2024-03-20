476911bd384a59969de39b7202867a237ee0b16a
Tervers <tcf847@protonmail.com>
2024-03-20 03:05:56 +0000

    further studying

--- c.c
+++ c.c
@@ -1,17 +1,18 @@
 /*
 
-#include <stdio.h>                                       // # = Directive.   include = preprocessor
+#include <stdio.h>         // # = Directive. Include = preprocessor type.
 
-int main(void)                                           // int = implies main returns an integer.   void = main has no argument.
+int main(void)             // int = variable type. implies main returns an integer / status code.
+                           // void = main has no argument.
 {
-printf("I love \n");                                     // printf = library function call.
-printf("you Michelle.\n");
-return 0;                                                // return = a statement to specify what value to return..
+printf("I love \n");       // printf = statement. library function call.
+printf("you Michelle.\n"); // statements end with a ;.
+return 0;                  // return = statement. value.
 }
 
 */
 
-// gcc -o c c.c                                          // -o = rename a.out to ...
+// gcc -o c c.c            // -o = rename a.out to ...
 
 // Preprocessing adds directives (usually handled by compiler)
 // Compiles into machine (object) code
@@ -19,15 +20,18 @@ return 0;                                                // return = a statement
 
 /*
 *
-* directives
+* #directives
 *
 * int main(void)
 *
 * {
 *
-*   statements
+*   declarations;          // int a;
+*   initialization;        // int a = 1, b = 2;
+*   assignments;           // a = 1; b = 2;
+*
+*   statements;            // printf("abc\n");
 *
 * }
 *
 */
-
