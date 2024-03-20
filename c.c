94012fb7630a91ac237a4ac24c3f34f09a87890a
Tervers <tcf847@protonmail.com>
2024-03-20 21:12:56 +0000

    #define

--- c.c
+++ c.c
@@ -72,7 +72,7 @@ int main(void)
 
 // printf("%d\n", height * length * width);
 
-
+/*
 
  #include <stdio.h>
 
@@ -95,5 +95,31 @@ int main(void)
    printf("Dimensional weight (pounds): %d\n", weight);
 
    return 0;
+
+}
+
+*/
+
+
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
+
+{
+	float fahrenheit, celsius;
+
+	printf("Enter Fahrenheit temperature: ");
+
+	scanf("%f", &fahrenheit);
+
+	celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+
+	printf("Celcius equivalent: %.1f\n", celsius);
+
+	return 0;
 
 }
