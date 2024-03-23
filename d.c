97f1a76fcfddd8a45a334e79bf37bd107c61fb60
Tervers <tcf847@protonmail.com>
2024-03-22 22:42:44 -0700

    more exercises

--- d.c
+++ d.c
@@ -173,3 +173,19 @@ int main(void)
 // printf("Weight: %d\n", (volume + 165) / 166);
 
 
+#include <stdio.h>
+
+int main()
+{
+	int radius;
+	float equation = (4.0f / 3.0f);
+	float pi = 3.1415;
+
+	printf("Enter radius (meters): \n");
+	scanf("%d", &radius);
+
+	printf("The volume of a sphere with radius of %d meters is %f.\n", radius, (pi * equation * (radius * radius * radius)));
+
+	return 0;
+
+}
