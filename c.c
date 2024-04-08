99ab0c47422e95526b4bf1c508f114da89bcb3cd
Trevor Freeman <tcf847@protonmail.com>
2024-04-08 11:17:38 -0700

    ch5p11

--- c.c
+++ c.c
@@ -2233,3 +2233,64 @@ int main(void)
 11. Write a program that asks the user for a two-digit number, then prints the
      English word for the number.
 
+#include <stdio.h>
+
+int main(void)
+{
+    int d1, d2;
+
+    printf("Enter a two-digit number: ");
+    scanf("%1d%1d", &d1, &d2);
+
+    if (d1 == 1)
+        if (d2 == 0)
+            printf("ten\n");
+        else if (d2 == 1)
+            printf("eleven\n");
+        else if (d2 == 2)
+            printf("twelve\n");
+        else switch (d2) {
+            case 3: printf("thirteen\n");       break;
+            case 4: printf("fourteen\n");       break;
+            case 5: printf("fifteen\n");       break;
+            case 6: printf("sixteen\n");       break;
+            case 7: printf("seventeen\n");       break;
+            case 8: printf("eightteen\n");       break;
+            case 9: printf("nineteen\n");       break;
+            }
+
+    else if (d1 >= 2 && 9 >= d1)
+        switch (d1) {
+            case 2: printf("twenty");            break;
+            case 3: printf("thirty");            break;
+            case 4: printf("forty");            break;
+            case 5: printf("fifty");            break;
+            case 6: printf("sixty");            break;
+            case 7: printf("seventy");            break;
+            case 8: printf("eighty");            break;
+            case 9: printf("ninety");            break;
+            }
+
+    if (d1 >= 2 && d2)
+        printf("-");
+
+    if (d1 != 1)
+        switch (d2) {
+            case 1: printf("one\n");            break;
+            case 2: printf("two\n");            break;
+            case 3: printf("three\n");            break;
+            case 4: printf("four\n");            break;
+            case 5: printf("five\n");            break;
+            case 6: printf("six\n");            break;
+            case 7: printf("seven\n");            break;
+            case 8: printf("eight\n");            break;
+            case 9: printf("nine\n");            break;
+            }
+
+    return 0;
+}
+
+/******************************************************************************
+*   CHAPTER                      6                                    LOOPS   *
+******************************************************************************/
+
