c14f2719db77dd5f4852a1ea45591533e9749cab
Tervers <tcf847@protonmail.com>
2024-05-15 17:31:05 -0700

    c8p8

--- chapter8.c
+++ chapter8.c
@@ -793,3 +793,56 @@ int main(void)
      students, then computes the total score and average score for each
      student, and the average score, high score, and low score for each quiz.
 
+#include <stdio.h>
+
+int main(void)
+{
+    int s, q, n;
+    int sq[5][5] = {0}, stot[5] = {0}, savg[5] = {0};
+    int qavg[5] = {0}, qtot[5] = {0};
+    int qh[5] = {0}, ql[5] = {100, 100, 100, 100, 100};
+
+    for (s = 0, n = 1; s <= 4; n++, s++) {
+        printf("Enter student %d quiz grades: ", n);
+        for (q = 0; q <= 4; q++) {
+            scanf("%d", &sq[s][q]);
+        }
+    }
+
+    for (s = 0; s <= 4; s++) {
+        for (q = 0; q <= 4; q++) {
+            stot[s] += sq[s][q];
+            qtot[s] += sq[q][s];
+            if (sq[q][s] > qh[s])
+                qh[s] = sq[q][s];
+            if (sq[q][s] < ql[s])
+                ql[s] = sq[q][s];
+        }
+        savg[s] = (stot[s] / 5);
+        qavg[s] = (qtot[s] / 5);
+    }
+
+    printf("Student totals:");
+    for (s = 0; s <= 4; s++)
+        printf(" %d", stot[s]);
+
+    printf("\nStudent averages:");
+    for (s = 0; s <= 4; s++)
+        printf(" %d", savg[s]);
+
+    printf("\nQuiz averages:");
+    for (q = 0; q <= 4; q++)
+        printf(" %d", qavg[q]);
+
+    printf("\nQuiz high scores:");
+    for (q = 0; q <= 4; q++)
+        printf(" %d", qh[q]);
+
+    printf("\nQuiz low scores:");
+    for (q = 0; q <= 4; q++)
+        printf(" %d", ql[q]);
+
+    printf("\n");
+
+    return 0;
+}
