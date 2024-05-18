42550841b13aabe606ecbd138ed22db020f599f8
Tervers <tcf847@protonmail.com>
2024-05-18 16:56:17 +0000

    c8p14

--- d.c
+++ d.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
-    short i;
-    char ch, term, sentence[len];    //terminating character, sentence [length]
+    bool fsc = 0; //first space check
+    short i, len;
+    char ch, term;
+    char word[i], sentence[len];//terminating character, sentence [length]
 
     printf("Enter a sentence: ");
 
-    for (;;len++) {
+    for (len = 0; ;) {
+
         ch = getchar();
-        if (ch == '\n')
-            break;
+        if (ch == '\n') {
+
+    	    for (i = 0; len >= 0; i++, len--) {
+
+        	ch = sentence[len];
+		word[i] = ch;
+		if (ch == ' ') {
+
+	            for (; i >= 0; i--) {
+		        putchar(word[i]);
+		    }
+
+	        }
+
+	    }
+	    break;
+        }
+
         else if (ch == '.' || ch == '!' || ch == '?')
             term = ch;
-        sentence[len] = ch;
+
+	else {
+            sentence[len] = ch;
+	    len++;
+	}
+
     }
+
+
+    putchar(term);
+
+    return 0;
 }
