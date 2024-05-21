befbb7c3302c181c8228878cc2f53dc7562f90c5
Tervers <tcf847@protonmail.com>
2024-05-21 12:13:14 -0700

    c9.2 notes

--- chapter09.c
+++ chapter09.c
@@ -20,7 +20,7 @@ printf("Average: %g\n", average(x, y)); //(x, y) function arguments
 
 function arguments can be a variable or any expression of a compatible type
 
-//			    average.c - COMPUTES AVERAGES OF THREE NUMBERS
+/***             average.c - COMPUTES AVERAGES OF THREE NUMBERS            ***/
 
 #include <stdio.h>
 
@@ -42,7 +42,26 @@ int main(void)
 	return 0;
 }
 
-//			    pun2.c - PRINTS A BAD PUN
+/***                    countdown.c - PRINTS A COUNTDOWN                   ***/
+
+#include <stdio.h>
+
+void print_count(int n)
+{
+    printf("T minus %d and counting\n", n);
+}
+
+int main(void)
+{
+    int i;
+
+    for (i = 10; i > 0; --i)
+        print_count(i);   //i gets copied to n in print_count()
+
+    return 0;
+}
+
+/***                       pun2.c - PRINTS A BAD PUN                       ***/
 
 #include <stdio.h>
 
@@ -57,4 +76,103 @@ int main(void)
 	return 0;
 }
 
+//                          FUNCTION DEFINITIONS
 
+return-type function-name ( parameters )
+{
+    declarations
+    statements
+}
+
+in C89, if the return type is omitted, the function is presumed to return a
+    value of type int
+in C99, it is illegal to omit the return type of a function
+
+double//legal placement of return-type
+average(double a, double b)
+{
+    return (a + b) / 2;
+}
+
+if a function has no parameters, it must contain void between the parentheses
+
+double average(double a, double b)
+{
+    double sum;     /* declaration */
+
+    sum = a + b;    /* statement */
+    return sum / 2; /* statement */
+}
+
+variables declared in the body of a function belong exclusively to that
+    function; they cannot be examined or modified by other functions
+in C89, variable declaration must come first, before all statements in the body
+    of a function
+in C99, variable declarations and statements can be mixed, as long as each
+    variable is declared prior to the first statement that uses the variable
+
+void print_pun(void)
+{
+}
+
+//                          FUNCTION CALLS
+
+average(x, y)       //*
+print_count(i)      //individual function calls
+print_pun()         //*
+
+a call of a void function is always follwed by a semicolon to turn it into a
+    statement:
+
+print_count(i);
+print_pun();
+
+a call of a non-void function produces a value that can be stored in a
+    variable, tested, printed, or used in some other way:
+
+avg = average(x, y);
+if (average(x, y) > 0)
+    printf("Average is positive\n");
+printf("The average is %g\n", average(x, y));
+
+/* discarding a return value ("expression statement")
+average(x, y);  //computes a value, but is not stored in a variable */
+
+num_chars = printf("Hi, Mom!\n");   //num_chars will have the value 9
+
+printf("Hi, Mom!\n");   //discards return value (9)
+
+(void) printf("Hi, Mom!\n");//shows we are deliberately discarding return value
+
+/***               prime.c - TESTS WHETHER A NUMBER IS PRIME               ***/
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool is_prime(int n)
+{
+    int divisor;
+
+    if (n <= 1)
+        return false;
+    for (divisor = 2; divisor * divisor <= n; divisor++)
+        if (n % divisor == 0)
+            return false;
+    return true;
+}
+
+int main(void)
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (is_prime(n))
+        printf("Prime\n");
+    else
+        printf("Not prime\n");
+
+    return 0;
+}
+
+/*** CHAPTER                   9.2 FUNCTION DECLARATIONS                   ***/
