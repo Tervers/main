80122706b7ba91b0f963e93fb450dc6840cbede1
Tervers <tcf847@protonmail.com>
2024-05-21 16:29:55 +0000

    c9 notes

--- chapter09.c
+++ chapter09.c
@@ -9,3 +9,52 @@ functions are reusable, allowing us to take a function that was part of one
 
 /*** CHAPTERS              9.1              DEFINING AND CALLING FUNCTIONS ***/
 
+//type function_name(function parameters)
+double average(double a, double b) //identifiers a and b
+{
+    return (a + b) / 2; //body
+}
+
+//function call average()
+printf("Average: %g\n", average(x, y)); //(x, y) function arguments
+
+function arguments can be a variable or any expression of a compatible type
+
+//			    average.c - COMPUTES AVERAGES OF THREE NUMBERS
+
+#include <stdio.h>
+
+double average(double a, double b)
+{
+	return (a + b) / 2;
+}
+
+int main(void)
+{
+	double x, y, z;
+
+	printf("Enter three numbers: ");
+	scanf("%lf%lf%lf", &x, &y, &z);
+	printf("Average of %g and %g: %g\n", x, y, average(x, y));
+	printf("Average of %g and %g: %g\n", y, z, average(y, z));
+	printf("Average of %g and %g: %g\n", x, z, average(x, z));
+
+	return 0;
+}
+
+//			    pun2.c - PRINTS A BAD PUN
+
+#include <stdio.h>
+
+void print_pun(void)
+{
+	printf("To C or not to C: that is the question.\n");
+}
+
+int main(void)
+{
+	print_pun();
+	return 0;
+}
+
+
