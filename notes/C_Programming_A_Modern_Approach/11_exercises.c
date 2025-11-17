a9f12b569331766333719efcc54e317c5b1c9713
Tervers <124096481+Tervers@users.noreply.github.com>
2025-11-17 14:55:00 -0800

    finished C11 exercises

--- notes/C_Programming_A_Modern_Approach/11_exercises.c
+++ notes/C_Programming_A_Modern_Approach/11_exercises.c
@@ -132,7 +132,101 @@ day_of_year is an integer between 1 and 366, specifying a particular day within
 	function will store the equivalent month (1-12) and day within that month
 	(1-31).
 
+//some bugs. should rewrite to assume leap year and fix the rest from there.
+Answer:
+
+void split_date(int day_of_year, int year, int *month, int *day)
+{
+    if (day_of_year <= 31) {
+        *month = 1;
+        *day = day_of_year;
+    }
+    else if (day_of_year >= 32 && day_of_year <= 59) {
+        *month = 2;
+        *day = day_of_year - 31;
+    }
+    else if (day_of_year >= 60 && day_of_year <= 90) {
+        *month = 3;
+        *day = day_of_year - 59;
+    }
+    else if (day_of_year >= 91 && day_of_year <= 120) {
+        *month = 4;
+        *day = day_of_year - 90;
+    }
+    else if (day_of_year >= 121 && day_of_year <= 151) {
+        *month = 5;
+        *day = day_of_year - 120;
+    }
+    else if (day_of_year >= 152 && day_of_year <= 181) {
+        *month = 6;
+        *day = day_of_year - 151;
+    }
+    else if (day_of_year >= 182 && day_of_year <= 212) {
+        *month = 7;
+        *day = day_of_year - 181;
+    }
+    else if (day_of_year >= 213 && day_of_year <= 243) {
+        *month = 8;
+        *day = day_of_year - 212;
+    }
+    else if (day_of_year >= 244 && day_of_year <= 273) {
+        *month = 9;
+        *day = day_of_year - 243;
+    }
+    else if (day_of_year >= 274 && day_of_year <= 304) {
+        *month = 10;
+        *day = day_of_year - 273;
+    }
+    else if (day_of_year >= 305 && day_of_year <= 334) {
+        *month = 11;
+        *day = day_of_year - 304;
+    }
+    else if (day_of_year >= 335 && day_of_year <= 365) {
+        *month = 12;
+        *day = day_of_year - 334;
+    }
+
+    if ((year % 4) && day_of_year >= 60 && (!(year % 100))
+            || ((year % 400) && day_of_year >= 60)) {
+        *day = *day - 1;
+        if (*day == 0) {
+            *month = *month - 1;
+            switch (*month) {
+                case 2: *day = 29; break;
+                case 3: *day = 31; break;
+                case 4: *day = 30; break;
+                case 5: *day = 31; break;
+                case 6: *day = 30; break;
+                case 7: *day = 31; break;
+                case 8: *day = 31; break;
+                case 9: *day = 30; break;
+                case 10: *day = 31; break;
+                case 11: *day = 30; break;
+            }
+        }
+    }
+}
+
+
+
+8. Write the following function:
+    int *find_largest(int a[], int n);
+
+When passed an array of length n, the function will return a pointer to the
+    array's largest element.
+
 
 Answer:
 
-
+int *find_largest(int a[], int n)
+{
+    int largest = a[0], element = 0, i;
+    
+    for (i = 0; i < n; i++)
+        if (a[i] > largest) {
+            largest = a[i];
+            element = i;
+        }
+    
+    return &a[element];
+}
