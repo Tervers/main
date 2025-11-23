6021d85900c18858b297da34dc788933579a2408
Tervers <124096481+Tervers@users.noreply.github.com>
2025-11-23 15:10:01 -0800

    worked on C12e14

--- notes/C_Programming_A_Modern_Approach/Chapter_12/exercises/14/14.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/exercises/14/14.c
@@ -5,5 +5,29 @@ Assume that the following array contains a week's worth of hourly temperature
 
 int temperatures[7][24];
 
-Write a statement that uses the searrch function (see Exercise 7) to search the
+Write a statement that uses the search function (see Exercise 7) to search the
     entire temperatures array for the value 32.
+
+*/
+
+bool search(const int a[], int n, int key)
+{
+    const int *p = a;
+
+    for (p = a; p < a + n; p++)
+        if (key == *p)
+            return true;
+
+    return false;
+}
+
+int main(void)
+{
+    int key;
+    const int temperatures[7][24];
+
+    printf("What temperature are you looking for?");
+    scanf("%d", &key);
+
+    search(temperatures[0], 7 * 24, key);
+}
