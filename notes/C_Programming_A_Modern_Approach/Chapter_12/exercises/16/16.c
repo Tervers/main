2aab982714bb5ed0962d1fdf40126e34cdc3ba43
Tervers <124096481+Tervers@users.noreply.github.com>
2025-11-24 10:39:03 -0800

    added C12e16 template

---
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/exercises/16/16.c
@@ -0,0 +1,8 @@
+/*
+
+Write a loop that prints the highest temperature in the temperatures array
+    (see Exercise 14) for each day of the week. The loop body should call the
+    find_largest function, passing it one row of the array at a time.
+
+*/
+
