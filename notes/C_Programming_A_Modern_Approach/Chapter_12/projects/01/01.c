b79193d0ca8516687216f9a9c8ead21a91dd1e28
Tervers <124096481+Tervers@users.noreply.github.com>
2025-12-10 10:45:32 -0800

    finished C12p01

--- notes/C_Programming_A_Modern_Approach/Chapter_12/projects/01/01.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/projects/01/01.c
@@ -3,7 +3,7 @@
 (a) Write a program that reads a message, then prints the reversal of the
 message:
 
-    Enter a message: __Don't get mad, get even.__
+    Enter a message: __Don't_get_mad,_get_even.__
     Reversal is: .neve teg ,dam teg t'noD
 
 Hint: Read the message one character at a time (using getchar) and store the
@@ -16,3 +16,62 @@ the current position in the array.
 */
 
 
+(a)
+
+#include <stdio.h>
+
+#define LIMIT 80
+
+void reversed(char message[], int length)
+{
+    for(length -= 1; length >= 0; length--)
+        printf("%c", message[length]);
+
+    printf("\n");
+}
+
+int main(void)
+{
+    char message[LIMIT];
+    int length = 0;
+
+    printf("Enter a message to see it reversed:\n");
+
+    for(; length < LIMIT && message[length - 1] != '\n'; length++)
+        message[length] = getchar();
+
+    reversed(message, --length);
+}
+
+
+
+(b)
+
+#include <stdio.h>
+
+#define LIMIT 80
+
+int main(void)
+{
+    char message[LIMIT], *p;
+
+    printf("Enter a message to see it reversed:\n");
+
+    for(p = message; p < message + LIMIT; p++) {
+        *p = getchar();
+        if (*p == '\n')
+            break;
+    }
+
+    printf("In reverse:\n");
+
+    for (; p >= message; p--) {
+        if (*p == '\n')
+            --p;
+        putchar(*p);
+    }
+
+    printf("\n");
+
+    return 0; 
+}
