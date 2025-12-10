d38a35b8897a1805629ebf935d1eed767c2f8da5
Tervers <124096481+Tervers@users.noreply.github.com>
2025-12-09 20:13:34 -0800

    started C12p01

---
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/projects/01/01.c
@@ -0,0 +1,18 @@
+/*
+
+(a) Write a program that reads a message, then prints the reversal of the
+message:
+
+    Enter a message: __Don't get mad, get even.__
+    Reversal is: .neve teg ,dam teg t'noD
+
+Hint: Read the message one character at a time (using getchar) and store the
+characters in an array. Stop reading when the array is full or the character is
+'\n'.
+
+(b) Revise the program to use a pointer instead of an integer to keep track of
+the current position in the array.
+
+*/
+
+
