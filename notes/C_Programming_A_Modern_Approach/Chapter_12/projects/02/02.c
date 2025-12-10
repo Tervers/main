5670f88b811782db8d8fdb492883771eb3b9d1bd
Tervers <124096481+Tervers@users.noreply.github.com>
2025-12-10 11:47:18 -0800

    started C12p02

---
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/projects/02/02.c
@@ -0,0 +1,19 @@
+/*
+
+(a) Write a program that reads a message, then checks whether it's a palindrome:
+
+    Enter a message: __He_lived_as_a_devil,_eh?__
+    Palindrome
+
+    Enter a message: __Madam,_I_am_Adam.__
+    Not a palindrome
+
+Ignore all characters that aren't letters. Use integer variables to keep track
+of positions in the array.
+
+*/
+
+
+(a)
+
+
