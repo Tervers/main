0f968519decba6de23bd7a0b3e39c6f80eac0f0d
Tervers <124096481+Tervers@users.noreply.github.com>
2025-12-17 16:46:18 -0800

    actually finished C12p02

--- notes/C_Programming_A_Modern_Approach/Chapter_12/projects/02/02.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/projects/02/02.c
@@ -11,6 +11,9 @@
 Ignore all characters that aren't letters. Use integer variables to keep track
 of positions in the array.
 
+(b) Revise the program to use pointers instead of integers to keep track of pos-
+itions in the array.
+
 */
 
 
@@ -37,6 +40,42 @@ int main(void)
     p--;
 
     for (; q < message + i - 1; p--, q++)
+        if (*p != *q) {
+            mismatch = true;
+            break;
+        }
+
+    if (mismatch)
+        printf("Not a palindrome\n");
+    else if (!mismatch)
+        printf("Palindrome\n");
+
+    return 0;
+}
+
+
+//(b)
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 100
+int main(void)
+{
+    bool mismatch = false;
+    char message[MAX], *p = message, *q = message, *last;
+
+    printf("Enter a message to see if it is a palindrome:\n");
+    while ((*p = getchar()) != '\n') {
+	    if (*p >= 'a' && *p <= 'z' || *p >= 'A' && *p <= 'Z') {
+	        p++;
+        }
+    }
+
+    p--;
+    last = p;
+
+    for (; q < last; p--, q++)
         if (*p != *q) {
             mismatch = true;
             break;
