5ee8816c81c0647384ec8c3accb8a1e5891f7609
Tervers <124096481+Tervers@users.noreply.github.com>
2025-12-17 17:05:51 -0800

    started C12p07

---
+++ notes/C_Programming_A_Modern_Approach/Chapter_12/projects/05/05.c
@@ -0,0 +1,9 @@
+/*
+
+Modify Programming Project 14 from Chapter 8 so that it uses a pointer instead
+of an integer to keep track of the currect position in the array that contains
+the sentence.
+
+*/
+
+
