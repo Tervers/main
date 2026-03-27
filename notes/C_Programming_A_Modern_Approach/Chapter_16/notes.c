520ece22edfd3aa862a7482d2d2907a1424a1a2f
Tervers <124096481+Tervers@users.noreply.github.com>
2026-03-27 11:44:23 -0700

    added notes to C16

--- notes/C_Programming_A_Modern_Approach/Chapter_16/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_16/notes.c
@@ -122,10 +122,118 @@ the = operator can be used only with structures of "compatible" types. Two
     structures declared at the same time (as part1 and part2 were) are compat-
     ible.
 ! C provides no operations on entire structures. In particular, we can't use the
-    == and != operators to test whether two structures are equal or not equal.
+    == and != operators to test whether two structures are equal or not equal. !
 
 
 //              16.2 STRUCTURE TYPES
 
+
+if several structure variables with identical membersare declared at the same
+    time, there will be no problem. But if we need to declare the variables at
+    different points in the program, then problems arise:
+
+struct {
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+} part1;
+
+struct {
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+} part2;
+
+repeating structure information will bloat the program. Changing the program
+    later will be risky, since we can't easily gaurantee that the declarations
+    will remain consistent.
+the biggest problem is that part1 and part2 don't have compatible types. As a
+    result, part1 can't be assigned to part2, and vice versa.
+we also don't have a name for the type of part1 or part2, we can't use them as
+    arguments in function calls
+to avoid these problems, we have two ways to name structures:
+-declare a "structure tag"
+-use typedef to define a type name
+
+
+// Declaring a Structure Tag
+
+
+struct part {
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+};
+
+once we've created the part tag, we can use it to declare variables:
+
+struct part part1, part2;
+
+you can declare a structure tag with structure variables at the same time:
+
+struct part {
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+} part1, part2;
+
+all structures declared to have type struct part are compatible with one another
+
+struct part part1 = {528, "Disk drive", 10};
+struct part part2;
+
+part2 = part1;  /* legal */
+
+
+// Defining a Structure Type
+
+
+typedef struct {
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+} Part;
+
+declaring variables:
+
+Part part1, part2;
+
+we can choose either to declare a structure tag or to use typedef. But when 
+    a structure is to be used in a linked list, using a structure tag is manda-
+    tory.
+
+
+// Structures as Arguments and Return Values
+
+
+void print_part(struct part p)
+{
+    printf("Part number: %d\n", p.number);
+    printf("Part name: %s\n", p.name);
+    printf("Quantity on hand: %d\n", p.on_hand);
+}
+
+a call of print_part might look like:
+
+print_part(part1);
+
+a second function that returns a part structure that it constructs from its
+    arguments:
+
+struct part build_part(int number, const char *name, int on_hand)
+{
+    struct part p;
+
+    p.number = number;
+    strcpy(p.name, name);
+    p.on_hand = on_hand;
+    return p;
+}
+
+it's legal for build_part's parameters to have names that match the members of
+    the part structure, since the structure has its own name space
+here's how build_part might be called:
+
+part1 = build_part(528, "Disk drive", 10);
 
 
