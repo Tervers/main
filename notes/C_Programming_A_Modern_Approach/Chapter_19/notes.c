d8662c6a1e41b51ad794e2ca5425bf2cda20e4b0
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-03 21:38:22 -0700

    added notes to C19.2

--- notes/C_Programming_A_Modern_Approach/Chapter_19/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_19/notes.c
@@ -107,4 +107,73 @@ In C, the major tool for enforcing information hiding is the static storage
 // A Stack Module
 
 
+To see the benefits of information hiding, let's look at two implementations of
+	a stack module, one using an array and the other a linked list. The module's
+	header file will have the following appearance:
+
+
+//				stack.h
+
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(int i);
+int pop(void);
+
+#endif
+
+
+//				stack1.c
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+#define STACK_SIZE 100
+
+static int contents[STACK_SIZE];
+static int top = 0;
+
+static void terminate(const char *message)
+{
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+void make_empty(void)
+{
+	top = 0;
+}
+
+bool is_empty(void)
+{
+	return top == 0;
+}
+
+bool is_full(void)
+{
+	return top == STACK_SIZE;
+}
+
+void push(int i)
+{
+	if (is_full())
+		terminate("Error in push: stack is full.");
+	contents[top++] = i;
+}
+
+int pop(void)
+{
+	if (is_empty())
+		terminate("Error in pop: stack is empty.");
+	return contents[--top];
+}
+
 
