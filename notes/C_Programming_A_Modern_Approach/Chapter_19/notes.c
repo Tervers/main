35cf947376384bdfa9a1ee1e7241149654f84c50
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-03 19:26:20 -0700

    added notes to C19.2

--- notes/C_Programming_A_Modern_Approach/Chapter_19/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_19/notes.c
@@ -59,4 +59,52 @@ In our calculator problem, the stack module is clearly cohesive: its functions
 
 Because of the need for high cohesion and low coupling, modules tend to fall in-
     to certain typical categories:
-> A "data pool" is a collection of related variables and/or constants.
+> A "data pool" is a collection of related variables and/or constants. In C, a
+	module of this type is often just a header file. From a design standpoint,
+	putting variables in header files isn't usually a good idea, but collecting
+	related constants in a header file can often be useful. In the C library,
+	<float.h> and <limits.h> are both data pools.
+> A "library" is a collection of related functions. The <string.h> header, for
+	example, is the interface to a library of string-handling functions.
+> An "abstract object" is a collection of functions that operate on a hidden
+	data structure. (In this chapter, the term "object" has a different meaning
+	than in the rest of the book. In C terminology, an object is simply a block
+	of memory that can store a value. In this chapter, however, an object is a
+	collection of data bundled with operations on the data. If the data is hid-
+	den, the object is "abstract.") The stack module we've been discussing be-
+	longs to this category.
+> An "abstract data type (ADT)" is a type whose representation is hidden. Client
+	modules can use the type to declare variables, but have no knowledge of the
+	structure of those variables. For a client module to perform an operation on
+	such a variable, it must call a function provided by the abstract data type
+	module. Abstract data types play a significant role in modern programming.
+
+
+//				19.2 INFORMATION HIDING
+
+
+A well-designed module often keeps some information secret from its clients.
+	Clients of our stack module, for example, have no need to know whether the
+	stack is stored in an array, in a linked list, or in some other form. Delib-
+	erately concealing information from clients of a module is known as "infor-"
+	"mation hiding." Information hiding has two primary advantages:
+> "Security": If clients don't know how the stack is stored, they won't be able
+	to corrupt it by tampering with its internal workings.
+> "Flexibility": Making changes -- no matter how large -- to a module's internal
+	workings won't be difficult. For example, we could implement the stack as an
+	array at first, then later switch to a linked list or other representation.
+	We'll have to rewrite the implementation of the module, of course, but -- if
+	the module was designed properly -- we won't have to alter the module's
+	interface.
+In C, the major tool for enforcing information hiding is the static storage
+	class. Declaring a variable with file scope to be static gives it internal
+	linkage, thus preventing it from being accessed from other files, including
+	clients of the module. (Declaring a function to be static is also useful --
+	the function can be directly ccalled only by other functions in the same
+	file.)
+
+
+// A Stack Module
+
+
+
