e4ac5875c60fef686faa4c2ae15aef719b0b072c
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-07 09:04:28 -0700

    added notes to C20

--- notes/C_Programming_A_Modern_Approach/Chapter_20/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_20/notes.c
@@ -23,3 +23,32 @@ The value of i << j is the result when the bits in i are shifted left by j
     tive, zeros are added at the left as needed. If i is a negative number, the
     result is implementation-defined; some implementations add zeros at the left
     end, while others preserve the sign bit by adding ones.
+For portability, it's best to perform shifts only on unsigned numbers.
+
+unsigned short i, j;
+
+i = 13;        // i is now 13 (binary 0000000000001101
+j = i << 2;    // j is now 52 (binary 0000000000110100
+j = i >> 2;    // j is now  3 (binary 0000000000000011
+
+As these examples show, neither operator modifies its operands. To modify a var-
+    iable by shifting its bits, we'd use the compound assignment operators <<=
+    and >>=:
+
+i = 13;        // i is now 13 (binary 0000000000001101
+i <<= 2;       // i is now 52 (binary 0000000000110100
+i >>= 2;       // i is now 13 (binary 0000000000001101
+
+! The bitwise shift operators have lower precendence than the arithmetic opera-
+    tors. For example, i << 2 + 1 means i << (2 + 1), not (i << 2) + 1. !
+
+
+// Bitwise Complement, And, Exclusive Or, and Inclusive Or
+
+
+~ bitwise complement
+& bitwise and
+^ bitwise exclusisve or
+| bitwise inclusive or
+
+the ~ operator is unary; the integer promotions are 
