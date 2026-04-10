f13719b5995e4af41ea3154550c2d5280f28ebe7
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-10 13:23:27 -0700

    added notes to C21.2

--- notes/C_Programming_A_Modern_Approach/Chapter_21/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_21/notes.c
@@ -78,5 +78,105 @@ The rules also apply to names that are set aside for future use. The complete
 // Functions Hidden by Macros
 
 
+It's common for C programmers to replace small functions by parameterized mac-
+    ros. This practice occurs even in the standard library. The C standard al-
+    lows headers to define macros that have the same names as library functions,
+    but protects the programmer by requiring that a true function be available
+    as well. As a result, it's not unusual for a library header to declare a
+    function and define a macro with the same name.
+We've already seen an example of a macro duplicating a library function. getchar
+    is a library function declared in the <stdio.h> header. It has the following
+    prototype:
 
+int getchar(void);
+
+<stdio.h> usually defines getchar as a macro as well:
+
+#define getchar() getc(stdin)
+
+By default, a call of getchar will be treated as a macro invocation (since macro
+    names are replaced during preprocessing).
+Most of the time, we're happy using a macro instead of a true function, because
+    it will probably make our program run faster. Occasionally, though, we want
+    a genuine function, perhaps to minimize the size of the executable code.
+If the need arises, we can remove a macro definition (thus gaining access to the
+    true function) by using the #undef directive. For example, we could undefine
+    the getchar macro after including <stdio.h>:
+
+#include <stdio.h>
+#undef getchar
+
+If getchar isn't! a macro, no harm has been done; #undef has no effect when giv-
+    en a name that's not defined as a macro.
+As an alternative, we can disable individual uses of a macro by putting paren-
+    theses around its name:
+
+ch = (getchar)();   // instead of ch = getchar();
+
+The preprocessor can't spot a parameterized macro unless its name is followed by
+    a left parenthesis. The compiler isn't so easily fooled, however; it can
+    still recognize getchar as a function.
+
+
+//              21.2 C89 LIBRARY OVERVIEW
+
+
+<assert.h> Diagnostics (24.1)
+    Contains only the assert macro, which allows us to insert self-checks into a
+        program. If any check fails, the program terminates.
+
+<ctype.h> Character Handling (23.5)
+    Provides functions for classifying characters and for converting letters
+        from lower case to upper case and vice versa.
+
+<errno.h> Errors (24.2)
+    Provides errno ('error number'), an lvalue that can be tested after a call
+        of certain library functions to see if an error occurred during the
+        call.
+
+<float.h> Characteristics of Floating Types (23.1)
+    Providese macros that describe the characteristics of floating types, in-
+        cluding range and accuracy.
+
+<limits.h> Sizes of Integer Types (23.2)
+    Provides macros that describe the characteristics of integer types (includ-
+        ing character types), including their maximum and minimum values.
+
+<locale.h> Localization (25.1)
+    Provides functions to help a program adapt its behavior to a country or oth-
+        er geographic region. Locale-specific behavior includes the way numbers
+        are printed, the format of monetary values, the character set, and the
+        appearance of the date and time.
+
+<math.h> Mathematics (23.3)
+    Provides common mathematical functions, including trigonometric, hyperbolic,
+        exponential, logarithmic, power, nearest integer, absolute value, and
+        remainder functions.
+
+<setjmp.h> Nonlocal Jumps (24.4)
+    Provides the setjmp and longjmp functions. setjmp 'marks' a place in a pro-
+        gram; longjmp can then be used to return to that place later. These
+        functions make it possible to jump from one function into another,
+        still-active function, bypassing the normal function-return mechanism.
+        setjmp and longjmp are used primarily for handling serious problems that
+        arise during program execution.
+
+<signal.h> Signal Handling (24.3)
+    Provides functions that deal with exceptional conditions (signals), includ-
+        ing interrupts and run-time errors. The signal function installs a func-
+        tion to be called if a given signal should occur later. The raise func-
+        tion causes a signal to occur.
+
+<stdarg.h> Variable Arguments (26.1)
+    Provides tools for writing functions that, like printf and scanf, can have a
+        variable number of arguments.
+
+<stddef.h> Common Definitions (21.4)
+    Provides definitions of frequently used types and macros.
+
+<stdio.h> Input/Output (22.1-22.8)
+    Provides a large assortment of input/output functions, including operations
+        on both sequential and random-access files.
+
+<stdlib.h> General Utilities (26.2)
 
