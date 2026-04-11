d1440a194e46b9844ea26fe8305fd3e7bba82555
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-11 12:09:42 -0700

    added notes to C22.1

--- notes/C_Programming_A_Modern_Approach/Chapter_22/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_22/notes.c
@@ -13,4 +13,36 @@ In C89, all standard input/output functions belong to <stdio.h>, but such is not
 //              22.1 STREAMS
 
 
+In C, the term "stream" means any source of input or any destination for output.
+	Many small programs, like the ones in previous chapters, obtain all their
+	input from one stream (usually associated with the keyboard) and write all
+	their output to another stream (usually associated with the screen).
+Larger programs may need additional streams. These streams often represent files
+	stored on various media (such as hard drives, CDs, DVDs, and flash memory),
+	but they could just as easily be associated with devices that don't store
+	files: network ports, printers, and the like. We'll concentrate on files,
+	since they're common and easy to understand. (I may even occasionally use
+	the term 'file' when I should say 'stream.') Keep in mind, however, that
+	many of the functions in <stdio.h> work equally well with all streams, not
+	just the ones that represent files.
+
+
+// File Pointers
+
+
+Accessing a stream in a C program is done through a "file pointer," which has
+	type FILE * (the FILE type is declared in <stdio.h>). Certain streams are
+	represented by file pointers with standard names; we can declare additional
+	file pointers as needed. For example, if a program needs two streams in add-
+	ition to the standard ones, it might contain the following declaration:
+
+FILE *fp1, *fp2;
+
+A program may declare any number of FILE * variables, although operating systems
+	usually limit the number of streams that can be open at one time.
+
+
+// Standard Streams and Redirection
+
+
 
