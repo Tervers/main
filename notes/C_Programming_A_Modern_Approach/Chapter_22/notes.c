02b9771dac7ac322bd80e097deeacd34c8e989af
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-13 18:46:20 -0700

    added notes to C22.8

--- notes/C_Programming_A_Modern_Approach/Chapter_22/notes.c
+++ notes/C_Programming_A_Modern_Approach/Chapter_22/notes.c
@@ -1045,4 +1045,103 @@ The file-positioning functions are best used with binary streams, by the way. C
     end of a text stream or to return to a place that was visited previously.)
     For binary streams, fseek isn't required to support calls in which whence is
     SEEK_END.
+The ftell functions returns the current file position as a long integer. (If an
+    error occurs, ftell returns -1L and stores an error code in errno.) The
+    value returned by ftell may be saved and later supplied to a call of fseek,
+    making it possible to return to a previous file position:
+
+long file_pos;
+...
+file_pos = ftell(fp);           // saves current position
+...
+fseek(fp, file_pos, SEEK_SET);  // returns to old position
+
+If fp is a binary stream, the call ftell(fp) returns the current file position
+    as a byte count, where zero represents the beginning of the file. If fp is a
+    text stream, however, ftell(fp) isn't necessarily a byte count. As a result,
+    it's nest not to perform arithmetic on values returned by ftell. For exam-
+    ple, it's not a good idea to subtract values returned by ftell to see how
+    far apart two file positions are.
+The rewind function sets the file position at the beginning. The call rewind(fp)
+    is nearly equivalent to fseek(fp, 0L, SEEK_SET). The difference is that re-
+    wind doesn't return a value but does clear the error indicator for fp.
+fseek and ftell have once problem: the'yre limited to files whose positions can
+    be stored in a long integer. For working with very large files, C provides
+    two additional functions: fgetpos and fsetpos. These functions can handle
+    large files because they use values of type fpos_t to represent file posi-
+    tions. An fpos_t value isn't necessarily an integer; it could be a struc-
+    ture, for instance.
+The call fgetpos(fp, &file_pos) stores the file position associated with fp in
+    the file_pos variable. The call fsetpos(fp, &file_pos) sets the file posi-
+    tion for fp to be the value stored in file_pos. (This value must have been
+    obtained by a previous call of fgetpos.) If a call of fgetpos or fsetpos
+    fails, it stores an error code in errno. Both functions return zero when
+    they succeed and a nonzero value when they fail.
+Here's how we might use fgetpos and fsetpos to save a file position and return
+    to it later:
+
+fpos_t file_pos;
+...
+fgetpos(fp, &file_pos);     // saves current position
+...
+fsetpos(fp, &file_pos);     // returns to old position
+
+
+// PROGRAM: Modifying a File of Part Records
+
+
+The following program opens a binary file containing part structures, reads the
+    structures into an array, sets the on_hand member of each structure to 0,
+    and then writes the structures back to the file. Note that the program opens
+    the file in "rb+" mode, allowing both reading and writing.
+
+
+//              invclear.c
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NAME_LEN 25
+#define MAX_PARTS 100
+
+struct part {
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+} inventory[MAX_PARTS];
+
+int num_parts;
+
+int main(void)
+{
+    FILE *fp;
+    int i;
+
+    if ((fp = fopen("inventory.dat", "rb+")) == NULL) {
+        fprintf(stderr, "Can't open inventory file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    num_parts = fread(inventory, sizeof(struct part), MAX_PARTS, fp);
+
+    for (i = 0; i < num_parts; i++)
+        inventory[i].on_hand = 0;
+
+    rewind(fp);
+    fwrite(inventory, sizeof(struct part), num_parts, fp);
+    fclose(fp);
+
+    return 0;
+}
+
+Calling rewind is critical, by the way. After the fread call, the file position
+    is at the end of the file. If we were to call fwrite without calling rewind
+    first, fwrite would add new data to the end of the file instead of overwrit-
+    ing the old data.
+
+
+//              22.8 STRING I/O
+
+
 
