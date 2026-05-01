257f79509b5e127bcec915645d06610c0c9b46c7
Tervers <124096481+Tervers@users.noreply.github.com>
2026-04-30 22:26:08 -0700

    added viewport functionality to tgame

--- tgame/tgame.cpp
+++ tgame/tgame.cpp
@@ -1,3 +1,5 @@
+//loadMedia functions share gTexture, needs fixing
+
 /** Includes **/
 
 
@@ -198,6 +200,22 @@ bool loadTextureMedia()
 	return success;
 }
 
+bool loadViewportMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load texture
+    gTexture = loadTexture( "media/09/viewport.png" );
+    if( gTexture == NULL )
+    {
+        printf( "Failed to load texture image!\n" );
+        success = false;
+    }
+
+    //Nothing to load
+    return success;
+}
 SDL_Surface* loadSurface( std::string path )
 {
 	//The Final optimized image
@@ -248,6 +266,10 @@ int main( int argc, char* args[] )
 		{
 			printf( "Failed to load texture media!\n" );
 		}
+        else if( !loadViewportMedia() )
+        {
+            printf( "Failed to load viewport media!\n" );
+        }
 		else
 		{			
 			//Main loop flag
@@ -273,25 +295,55 @@ int main( int argc, char* args[] )
                 SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
 
+                //Top left corner viewport
+                SDL_Rect topLeftViewport;
+                topLeftViewport.x = 0;
+                topLeftViewport.y = 0;
+                topLeftViewport.w = SCREEN_WIDTH / 2;
+                topLeftViewport.h = SCREEN_HEIGHT / 2;
+                SDL_RenderSetViewport( gRenderer, &topLeftViewport );
+
 				//Render texture to screen
 				SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
 
+                //Top right viewport
+                SDL_Rect topRightViewport;
+                topRightViewport.x = SCREEN_WIDTH / 2;
+                topRightViewport.y = 0;
+                topRightViewport.w = SCREEN_WIDTH / 2;
+                topRightViewport.h = SCREEN_HEIGHT / 2;
+                SDL_RenderSetViewport( gRenderer, &topRightViewport );
+
+                //Render texture to screen
+                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
+
+                //Bottom viewport
+                SDL_Rect bottomViewport;
+                bottomViewport.x = 0;
+                bottomViewport.y = SCREEN_HEIGHT / 2;
+                bottomViewport.w = SCREEN_WIDTH;
+                bottomViewport.h = SCREEN_HEIGHT / 2;
+                SDL_RenderSetViewport( gRenderer, &bottomViewport );
+
+                //Render texture to screen
+                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
+
                 //Render red filled quad
                 SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) };
-                SDL_SetRenderDrawColor( gRenderer, 0xff, 0x00, 0x00, 0xFF );
+                SDL_SetRenderDrawColor( gRenderer, 0xff, 0x00, 0x00, 0x7F );
                 SDL_RenderFillRect( gRenderer, &fillRect );
 
                 //Render green outlined quad
                 SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, (SCREEN_HEIGHT * 2 / 3) };
-                SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF );
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0x7F );
                 SDL_RenderDrawRect( gRenderer, &outlineRect );
 
                 //Draw blue horizontal line
-                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );
+                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0x7F );
                 SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
 
                 //Draw vertical line of yellow dots
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0x7F );
                 for( int i = 9; i < SCREEN_HEIGHT; i += 4 )
                 {
                     SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i );
