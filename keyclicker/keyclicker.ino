65c4cca8c2b74117848a8d964af66524f6321292
Tervers <124096481+Tervers@users.noreply.github.com>
2026-07-19 19:26:37 -0700

    Added 7-segment variables to keyclicker.ino

--- keyclicker/keyclicker.ino
+++ keyclicker/keyclicker.ino
@@ -1,25 +1,39 @@
+/* INCLUDES */
 #include <Servo.h>
 #include <time.h>
 #include <stdlib.h>
 
-#define SERVO_PIN 16
+/* DEFINES */
+#define SERVO_PIN 22
 #define PIN_ADC0  26
-#define ANGLE_L1  13    //led1 representing minAngle
-#define ANGLE_L2  14
-#define ANGLE_L3  15`    //GPIO subject to change
 
+/* OBJECTS */
 Servo mainServo;  // create servo object to control a servo
 
+/* SERVO VARIABLES */
 int pos = 0;         // variable to store the servo position
 int minAngle = 10;   // minimum angle servo can lower to
 int offset = 0;      // added time to randomize keyclicks
 
-bool counter[3] = {0, 0, 0};
+/* 4-DIGIT 7-SEGMENT DISPLAY VARIABLES */
+int dataPin = 18;   
+int latchPin = 20;
+int clockPin = 21;          
+int comPin[] = {17, 16, 15, 14};// Common pin (anode) of 4 digit 7-segment display
 
+byte num[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8,    // characters 0-F
+              0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
+
+/* VARIABILITY VARIABLES */
+int v;            // variability level
+bool counter[v];  
+
+/* INITIALIZATION */
 void setup() {
   mainServo.attach(SERVO_PIN, 500, 2500);  // attaches the servo on pin 9 to the servo object
 }
 
+/* MAIN */
 void loop() {
     //led and angle controls
 
