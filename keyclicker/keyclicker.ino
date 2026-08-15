57ad7160cc40e549b160de7b4379e45057a9e2ee
Tervers <124096481+Tervers@users.noreply.github.com>
2026-08-15 14:40:42 -0700

    added loop graphic to display during arm movement

--- keyclicker/keyclicker.ino
+++ keyclicker/keyclicker.ino
@@ -3,31 +3,34 @@
 #include <stdlib.h>
 #include <pico/time.h>
 
+/* Pin number defines */
 #define SERVO_PIN 22
 #define SERVO_ANALOG 26
 #define TIME_ANALOG 28
 #define OFFSET_SELECT_PIN 
 #define ANGLE_SELECT_PIN 13
 #define TIME_SELECT_PIN 19
-
-#define USER_MINIMUM_ANGLE  75
-#define USER_MAXIMUM_ANGLE 125
-
 #define DATA_PIN 18
 #define LATCH_PIN 20
 #define CLOCK_PIN 21
 const int DIGIT_PIN[] = {17, 16, 15, 14};
 
-void selectDigit(uint8_t digit);
+/* Servo arm angle restrictions */
+#define USER_MINIMUM_ANGLE  75
+#define USER_MAXIMUM_ANGLE 125
+
+/* Prototypes */
+void selectDigit(uint8_t value);
 void writeData(int value);
 void setDigits(int value);
 void writeDisplay(int value, int a[]);
 
+/* Servo control values */
 Servo mainServo;
-
 int pos = 0;
 int setAngle = USER_MINIMUM_ANGLE;
 
+/* Time control values */
 int timer = 0;
 int countdown = 0;
 int seconds = 0;
@@ -35,17 +38,40 @@ int offset = 0;
 uint32_t now = to_ms_since_boot(get_absolute_time());
 uint32_t stop = now + (seconds * 1000);
 
+/* Display control values */
 int digitValue;
 int digits[4];
 
+/* Loop control values */
 bool startToggle = false;
 bool timeToggle = false;
 bool angleToggle = false;
 bool offsetToggle = true;
-         
-uint8_t num[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07,    // characters 0-7
-              0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};    // characters 8-9, A-F
 
+/* Values that will draw a circle around the display */
+struct circle {
+  int digit;
+  uint8_t shape;
+} sections[12] = {
+  {.digit = 3, .shape = 0x02},
+  {.digit = 3, .shape = 0x04},
+  {.digit = 3, .shape = 0x08},
+  {.digit = 2, .shape = 0x08},
+  {.digit = 1, .shape = 0x08},
+  {.digit = 0, .shape = 0x08},
+  {.digit = 0, .shape = 0x10},
+  {.digit = 0, .shape = 0x20},
+  {.digit = 0, .shape = 0x01},
+  {.digit = 1, .shape = 0x01},
+  {.digit = 2, .shape = 0x01},
+  {.digit = 3, .shape = 0x01}
+};
+
+/* Hex values representing characters 0 through 9, then A through F */
+uint8_t num[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07,
+              0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
+
+/* Pin initialization */
 void setup() {
   mainServo.attach(SERVO_PIN, 500, 2500);
   pinMode(ANGLE_SELECT_PIN, INPUT_PULLUP);
@@ -61,33 +87,22 @@ void setup() {
 }
 
 void loop() {
-/*  
-  if (digitalRead(ANGLE_SELECT_PIN) == LOW) {
-    delay(20);
-    if (digitalRead(ANGLE_SELECT_PIN) == LOW) {
-      angleToggle = true;
-    }
-  }
-  if (digitalRead(TIME_SELECT_PIN) == LOW) {
-    delay(20);
-    if (digitalRead(TIME_SELECT_PIN) == LOW) {
-      timeToggle = true;
-    }
-  }
-*/
-  if (!timeToggle && !angleToggle)
-    startToggle = true;  
-
-  while (startToggle) {
-    setAngle = map(analogRead(SERVO_ANALOG), 0, 1023, USER_MAXIMUM_ANGLE, USER_MINIMUM_ANGLE);
-    seconds = map(analogRead(TIME_ANALOG), 0, 1023, 60, 0);
-    for (pos = setAngle; pos < setAngle + 6; pos += 1) {
+  while (!timeToggle && !angleToggle) {
+    for (int i = 0, pos = setAngle; pos < setAngle + 6; pos += 1, i++) {
       mainServo.write(pos);
-      delay(20);
+      for (int j = 0; j < 4; j++) {
+        selectDigit(sections[i].digit);
+        writeData(sections[i].shape);
+        delay(5);
+      }
     }
-    for (pos = setAngle; pos > setAngle - 6; pos -= 1) {
+    for (int i = 6, pos = setAngle; pos > setAngle - 6; pos -= 1, i++) {
       mainServo.write(pos);
-      delay(20);
+      for (int j = 0; j < 4; j++) {
+        selectDigit(sections[i].digit);
+        writeData(sections[i].shape);
+        delay(5);
+      }
     }
     if (offsetToggle == true) {
         srand((unsigned) time(NULL));
@@ -97,6 +112,7 @@ void loop() {
       offset = 0;
     now = to_ms_since_boot(get_absolute_time());
     timer = (now + (seconds * 1000) + offset);
+
     while (to_ms_since_boot(get_absolute_time()) < (now + (seconds * 1000) + offset)) {
       countdown = timer - to_ms_since_boot(get_absolute_time());
       digits[0] = ((countdown % 1000000) / 100000);
@@ -113,7 +129,11 @@ void loop() {
           writeData(num[digitValue]);
         }
         delay(5);
-        writeData(0xff);      
+        writeData(0x00);      
+      }
+      for (int i = 0; i < 4; i++) {
+        selectDigit(i);
+        writeData(0x00);
       }
       if (digitalRead(TIME_SELECT_PIN) == LOW) {
         delay(20);
@@ -132,9 +152,9 @@ void loop() {
         }
       }
     }
-
+  }
   delay(200);
-
+  
   while (timeToggle) {
     seconds = map(analogRead(TIME_ANALOG), 0, 1023, 60, 0);
     digits[1] = ((seconds % 1000) / 100);
@@ -145,15 +165,15 @@ void loop() {
       digitValue = digits[i];
       writeData(num[digitValue]);
       delay(5);
-      writeData(0xff);      
+      writeData(0x00);      
     }
     if (digitalRead(TIME_SELECT_PIN) == LOW) {
       delay(20);
       if (digitalRead(TIME_SELECT_PIN) == LOW)
         timeToggle = false;
-      }
+    }
   }
-
+  
   while (angleToggle) {
     setAngle = map(analogRead(SERVO_ANALOG), 0, 1023, USER_MAXIMUM_ANGLE, USER_MINIMUM_ANGLE);
     mainServo.write(setAngle);
@@ -165,23 +185,21 @@ void loop() {
       digitValue = digits[i];
       writeData(num[digitValue]);
       delay(5);
-      writeData(0xff);      
+      writeData(0x00);      
     }
     if (digitalRead(ANGLE_SELECT_PIN) == LOW) {
       delay(20);
       if (digitalRead(ANGLE_SELECT_PIN) == LOW)
         angleToggle = false;
-      }
+    }
   }
 }
-}
 
-
-void selectDigit(uint8_t digit) {
+void selectDigit(uint8_t value) {
   for (int i = 0; i < 4; i++) {
     digitalWrite(DIGIT_PIN[i], HIGH);  // Clear all 7-segment sections
   }
-  digitalWrite(DIGIT_PIN[digit], LOW);  // Open the selected individual 7-segment display
+  digitalWrite(DIGIT_PIN[value], LOW);  // Open the selected individual 7-segment display
 }
 
 void writeData(int value) {
